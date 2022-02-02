80eb0b73dea139f6df5c1c1f9faebd244a9ec41e
MatTyaN0125 <ilovegames.ilikesplatoon@gmail.com>
2022-02-02 17:02:44 +0900

    追加：DOTweenのアニメーションはあまり変化できなかった

--- Assets/Scripts/DoTweenScript.cs
+++ Assets/Scripts/DoTweenScript.cs
@@ -3,19 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 /// <summary>
-/// 
+/// DoTweenアニメーション
 /// </summary>
 public class DoTweenScript : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        transform.DORotate(new Vector3(0, 360f, 0), 10f, RotateMode.WorldAxisAdd);
+
         transform.DORotate(new Vector3(0, 360, 0), 10f, RotateMode.LocalAxisAdd);
+        //returnTitle.transform.DOLocalMoveY(0.5f,1f)/*.SetEase(Ease.InCubic)*/.SetLoops(2,LoopType.Yoyo).OnComplete(()=> { Debug.Log("owatta"); });
+        //returnTitle.transform.DOLocalMoveY(0f,1f)/*.SetEase(Ease.InCubic)*/.SetRelative().SetLoops(10,LoopType.Yoyo);
        
     }
 }
